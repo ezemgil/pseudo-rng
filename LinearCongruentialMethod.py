619313e7a8c4c8b89021e5b797fa35baf883c0dd
ezemgil <ezemgil@gmail.com>
2025-04-12 15:13:16 -0300

    Bug fixes

--- LinearCongruentialMethod.py
+++ LinearCongruentialMethod.py
@@ -1,3 +1,5 @@
+import math
+
 class LinearCongruentialMethod:
     def __init__(self, seed: int, a: int, m: int, c: int):
         self.seed = seed
@@ -32,21 +34,32 @@ class LinearCongruentialMethod:
         return [x / (self.m - 1) for x in numbers]
 
     def execute(self) -> None:
-        print("Método Congruencial Lineal")
-        self.seed = int(input("Semilla: "))
-        k = float(input("Constante multiplicativa: a = 1 +4k. Ingrese el valor de k: "))
-        g = int(input("Módulo: m = 2^g. Ingrese el valor de g: "))
-        self.c = int(input("Constante aditiva c (Debe ser relativamente primo a m): "))
-        n = int(input("Número de números pseudo aleatorios a generar: "))
+        def validate_positive_integer(value: int, name: str) -> int:
+            if value <= 0:
+                raise ValueError(f"{name} debe ser un número entero positivo.")
+            return value
+
+        def validate_c(value: int, m: int) -> int:
+            if value <= 0 or math.gcd(value, m) != 1:
+                raise ValueError("c debe ser un número entero positivo y relativamente primo a m.")
+            return value
+
+        print("\nMétodo Congruencial Lineal")
+        self.seed = validate_positive_integer(int(input("Semilla (X0, debe ser un número entero positivo): ")),
+                                              "Semilla")
+        k = validate_positive_integer(
+            int(input("Constante multiplicativa: a = 1 + 4k. Ingrese el valor de k (entero positivo): ")), "k")
+        g = validate_positive_integer(int(input("Módulo: m = 2^g. Ingrese el valor de g (entero positivo): ")), "g")
+        self.m = 2 ** g
+        self.c = validate_c(int(input("Constante aditiva c (Debe ser relativamente primo a m): ")), self.m)
 
         self.a = 1 + 4 * k
-        self.m = 2 ** g
 
         print("\nPeríodo máximo (2^g) = m: ", self.m)
 
-        numbers = self.random(n)
+        numbers = self.random(n := int(input("Número de números pseudo aleatorios a generar: ")))
 
         print("\ni\tℤ[0, ∞)\t\tℝ [0, 1)")
         print("-" * 30)
         for i, num in enumerate(numbers):
-            print(f"{i}\t{num * (self.m - 1)}\t\t\t{num:.6f}")
+            print(f"{i}\t{int(num * (self.m - 1)):>10}\t\t{num:.6f}")
